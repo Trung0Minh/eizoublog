43a61242a2125ed0feb064c62040b719f79cd620
trungminh <trungdeptrai81205@gmail.com>
2026-06-15 10:54:26 +0700

    fix: declare setImage command on CustomImageExtension for TS

--- components/editor/extensions/CustomImageExtension.ts
+++ components/editor/extensions/CustomImageExtension.ts
@@ -2,6 +2,14 @@ import { mergeAttributes, Node } from "@tiptap/core"
 import { ReactNodeViewRenderer } from "@tiptap/react"
 import { ImageNodeView } from "./ImageNodeView"
 
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    customImage: {
+      setImage: (options: { src: string; alt?: string }) => ReturnType
+    }
+  }
+}
+
 export const CustomImageExtension = Node.create({
   name: "customImage",
   group: "block",
