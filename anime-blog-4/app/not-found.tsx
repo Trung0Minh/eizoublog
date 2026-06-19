ab3148fe95012384cf9a136921681b4b1869efca
trungminh <trungdeptrai81205@gmail.com>
2026-06-19 21:08:11 +0700

    Remove local anime blog reference copies

--- anime-blog-4/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-[48px] font-bold">404</h1>
-      <p>Page could not be found.</p>
-    </div>
-  );
-}
