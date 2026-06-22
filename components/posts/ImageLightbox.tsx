4fc2c361a2f5264215747504706c80572505d061
trungminh <trungdeptrai81205@gmail.com>
2026-06-22 19:04:49 +0700

    Configure smooth mouse wheel zooming in ImageLightbox

--- components/posts/ImageLightbox.tsx
+++ components/posts/ImageLightbox.tsx
@@ -139,6 +139,7 @@ export function ImageLightbox({
         maxScale={4}
         centerOnInit
         doubleClick={{ mode: "zoomIn" }}
+        wheel={{ step: 0.05 }}
       >
         {({ zoomIn, zoomOut }) => (
           <>
